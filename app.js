f83a8f351de849ce3d789558b7b949c72a26a080
Administrator@CREECYNET.COM <Administrator@CREECYNET.COM>
2019-01-08 18:53:12 -0800

    Updates

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var app = express();
 app.get('/', function(req, res) {
 res.sendFile(__dirname + '/index.html');});
 app.use(express.static(__dirname + '/'));
-
 var port = 3000;
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
